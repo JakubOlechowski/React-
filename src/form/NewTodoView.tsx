447576acb53589fc24b099188ed53411330b9145
Jakub Olechowski <j.olechowski04@gmail.com>
2024-05-20 18:27:07 +0200

    added unfinished adding new todos list

--- src/form/NewTodoView.tsx
+++ src/form/NewTodoView.tsx
@@ -1,8 +1,34 @@
+import { useState } from "react"
 import NewTodoForm from "./NewTodoForm"
-
+type TodosListType = {
+  id: string
+  title: string
+}
 const NewTodoView = () => {
+  const [todosLists, setTodosLists] = useState<TodosListType[]>([])
+  const [newTodosList, setNewTodosList] = useState<TodosListType>({})
+
+  function addNewTodosList(newTodosListProp: TodosListType): void {
+    setTodosLists((prevState: TodosListType[]) => {
+      return [
+        ...prevState,
+        { id: newTodosListProp.id, title: newTodosListProp.title },
+      ]
+    })
+  }
+
   return (
     <>
+      <button onClick={() => addNewTodosList(newTodosList)} className="btn">
+        Add new list
+      </button>
+      {todosLists.map((todosList: TodosListType) => {
+        return (
+          <>
+            <NewTodoForm title={todosList.title} />
+          </>
+        )
+      })}
       <NewTodoForm />
     </>
   )
